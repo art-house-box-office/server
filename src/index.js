c0815a5aa510d0a2a4440e6a328403a584504d42
Johnny Luangphasy <johnny@jluangphasy.com>
2016-07-09 16:52:55 -0700

    Move dotenv

--- src/index.js
+++ src/index.js
@@ -1,14 +1,12 @@
 import dotenv from 'dotenv';
 import app from './lib/app';
 
-dotenv.config();
-
 const port = process.argv[2] || 9000;
+
+dotenv.config();
 
 app.listen(port, err => {
   if (err) return console.error(err);
 
-  console.log(process.env.FOO);
-  
   return console.log(`Magic happens at: http://localhost:${port}/`);
 });
