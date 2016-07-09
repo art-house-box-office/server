59f33d8ac7264caad771dfcc9b8178e0962780d4
DonChatelain <djchatelain@gmail.com>
2016-07-09 16:43:54 -0700

    adds dotenv package that loads .env file from within index.js - instead of relying on package.json cat command... ef that nonsense

--- src/index.js
+++ src/index.js
@@ -1,9 +1,14 @@
+import dotenv from 'dotenv';
 import app from './lib/app';
+
+dotenv.config();
 
 const port = process.argv[2] || 9000;
 
 app.listen(port, err => {
   if (err) return console.error(err);
 
+  console.log(process.env.FOO);
+  
   return console.log(`Magic happens at: http://localhost:${port}/`);
 });
