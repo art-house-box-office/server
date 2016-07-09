f6aea1cd7b4e69325b44875330e0ef5fa297b945
David <goodwid@gmail.com>
2016-07-09 16:09:31 -0700

    Added setup-mongoose.js

--- src/index.js
+++ src/index.js
@@ -4,6 +4,5 @@ const port = process.argv[2] || 9000;
 
 app.listen(port, err => {
   if (err) return console.error(err);
-
-  return console.log(`Magic happens at: http://localhost:${port}/`);
+  return console.log(`Server listening at: http://localhost:${port}/`);
 });
--- src/lib/app.js
+++ src/lib/app.js
@@ -1,11 +1,11 @@
 import express from 'express';
 import morgan from 'morgan';
-
+import cors from './cors';
 const app = express();
 export default app;
 
 app.use(morgan('dev'));
-
+app.use(cors('*'));
 // eslint-disable-next-line  no-unused-vars
 app.use((err, req, res, next) => {
   console.error(err);
