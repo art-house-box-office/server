87fecb6d8889f09eaf7e9feb414916f5f06dc55b
DonChatelain <djchatelain@gmail.com>
2016-07-11 10:45:45 -0700

    adds locations route

--- src/lib/app.js
+++ src/lib/app.js
@@ -1,12 +1,14 @@
 import express from 'express';
 import morgan from 'morgan';
 import cors from './cors';
+import locations from '../routes/locations';
 const app = express();
 export default app;
 
 app.use(morgan('dev'));
 app.use(cors('*'));
 
+app.use('/api/locations', locations);
 
 // eslint-disable-next-line  no-unused-vars
 app.use((err, req, res, next) => {
