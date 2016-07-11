fe32732f7ef6b6fa705b27385f74d2cf76401f18
Yvonne Hayes <queyvonne@gmail.com>
2016-07-11 10:27:57 -0700

    trying to set up endpoints for screenings

---
+++ src/routes/screenings.js
@@ -0,0 +1,52 @@
+const express = require('express');
+// const bodyParser = require('body-parser');
+const Screening = require('../models/screenings.model');
+const router = module.exports = express.router();
+// const jsonParser = bodyParser.json();
+
+router
+  // Retrieve all Screenings
+  .get('/', (req, res) => {
+    Screening
+      .find({})
+      .lean()
+      .then(screenings => {
+        const resObj = {
+          status: 'error',
+          result: 'No Screenings Added.',
+        };
+
+        if (screenings.length > 0) {
+          resObj.status = 'success';
+          resObj.result = screenings;
+        }
+
+        res.json(resObj);
+      });
+  })
+  // Retrieve a specific Screening
+  .get('/:screeningId', (req, res) => {
+    Screening
+      .findById(req.params.screeningId)
+      .lean()
+      .then(screening => {
+        const resObj = {
+          status: 'error',
+          result: `SCREENING NOT FOUND: ${screening.name} does not exist.`,
+        };
+
+        if (screening) {
+          resObj.status = 'success';
+          resObj.result = screening;
+        }
+
+        res.json(resObj);
+      })
+      .catch(err => {
+        res.json({
+          status: 'error',
+          result: 'Server error',
+          error: err,
+        });
+      });
+  });
